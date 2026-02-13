b18fcbe0143fe6b87f360d6e2bf28dcaebf7511b
ErickGiraldo12 <nietoerick52@gmail.com>
2026-02-13 17:22:59 -0500

    nuevo

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printl("PRIMEROS COMANDOS DE GIT");
+        System.out.printl("NOMBRES - JAVA ");
+        System.out.printl("1. git add");
+        System.out.printl("2. GIT COMMI -M");
+        System.out.printl("3.GIT");
+        System.out.printl("4. switch");
 
 
-        }
+    }
     }
